a21e191929a8d9be55ef074cb03e405dca6ee4da
Ram Chandra <chandraram8178@gmail.com>
2025-11-22 01:05:17 +0530

    Remove SiTwitter import from page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { DiMongodb, DiNginx, DiNpm, DiPostgresql, DiVim } from "react-icons/di";
-import { SiVscode, SiTwitter } from "react-icons/si";
+import { SiVscode } from "react-icons/si";
 
 import {
   FaAws,
